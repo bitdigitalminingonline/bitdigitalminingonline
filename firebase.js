b6486f41e7a161e51d5fd0467a299968d50338cc
bitdigitalmining <invesonbitcoinmining@gmail.com>
2025-03-07 21:17:06 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,14 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVzBsLYYjQDHPO6GXelLV9pIOu-nmzqnY",
-  authDomain: "earn-money-online-9f77b.firebaseapp.com",
-  databaseURL: "https://earn-money-online-9f77b-default-rtdb.firebaseio.com",
-  projectId: "earn-money-online-9f77b",
-  storageBucket: "earn-money-online-9f77b.firebasestorage.app",
-  messagingSenderId: "119190206068",
-  appId: "1:119190206068:web:20c136c3c972039eacc321",
-  measurementId: "G-W6B3H6TQ8V"
+  apiKey: "AIzaSyCURKSGmM5EqxIVbQeZ08hZkmQilO0xDSE",
+  authDomain: "signup-7dbdd.firebaseapp.com",
+  projectId: "signup-7dbdd",
+  storageBucket: "signup-7dbdd.firebasestorage.app",
+  messagingSenderId: "494207098434",
+  appId: "1:494207098434:web:2b83f29b79da934e62d7db",
+  measurementId: "G-T433YTY03M"
 };
 
 // Initialize Firebase
